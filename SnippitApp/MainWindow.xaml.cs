7e2b296aede85a31e802eec209c113ba49d41aff
Seba Kesteloot <seba.kesteloot@gmail.com>
2021-02-03 18:56:55 +0100

    cleanup

--- SnippitApp/MainWindow.xaml.cs
+++ SnippitApp/MainWindow.xaml.cs
@@ -19,7 +19,6 @@ namespace SnippitApp
             InitializeComponent();
             _repo = SnippitRepo.GetSnippetRepo();
             SnippitList = _repo.GetSnippits();
-            //SnippitList = FillListWithSnippits();
             FillListBoxItems();
         }
 
@@ -40,43 +39,7 @@ namespace SnippitApp
                 MainSnipWindow.Text = displaySnippit.SnipContent;
             }
             
-        }
-
-        //private List<CodeSnippit> FillListWithSnippits()
-        //{
-        //    List<CodeSnippit> snippitlistrepo = new List<CodeSnippit>();
-        //    JsonWriter fuckingdoit = new JsonWriter();
-
-        //    snippitlistrepo.Add(new CodeSnippit("ThisSnippit", "You", "Summary", "Content"));
-        //    snippitlistrepo.Add(new CodeSnippit("ThisSnippit2", "You2", "Summary2", "Content2"));
-        //    snippitlistrepo.Add(new CodeSnippit("ThisSnippit3", "You3", "Summary3", "Content3"));
-        //    snippitlistrepo.Add(new CodeSnippit("ThisSnippit4", "You4", "Summary4", "Content4"));
-        //    snippitlistrepo.Add(new CodeSnippit("ThisSnippit5", "You5", "Summary5", "Content5"));
-        //    snippitlistrepo.Add(new CodeSnippit("ThisSnippit6", "You6", "Summary6", "Content6"));
-        //    snippitlistrepo.Add(new CodeSnippit("ThisSnippit7", "You7", "Summary7", "Content7"));
-        //    snippitlistrepo.Add(new CodeSnippit("ThisSnippit8", "You8", "Summary8", "Content8"));
-        //    snippitlistrepo.Add(new CodeSnippit("ThisSnippit9", "You9", "Summary9", "Content9"));
-        //    snippitlistrepo.Add(new CodeSnippit("ThisSnippit10", "You10", "Summary10", "Content10"));
-        //    snippitlistrepo.Add(new CodeSnippit("ThisSnippit11", "You11", "Summary11", "Content11"));
-
-        //    fuckingdoit.ToJson(snippitlistrepo);
-
-        //    return snippitlistrepo;
-        //}
-
-        //private string GetSnippitCONTENTOnId(int id)
-        //{
-        //    string snippit = "";
-        //    foreach (var item in SnippitList)
-        //    {
-        //        if (item.SnipID == id)
-        //        {
-        //            snippit = item.SnipContent;
-        //            break;
-        //        }                
-        //    }
-        //    return snippit;
-        //}
+        }        
 
         private void MenuItemNew_Click(object sender, RoutedEventArgs e)
         {
@@ -90,8 +53,7 @@ namespace SnippitApp
         {
             ListBoxOverView.Items.Clear();
             FillListBoxItems();
-        }
-        
+        }     
 
 
     }
